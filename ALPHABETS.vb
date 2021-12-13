b77e0d79a5607bdd7f384c8df06cfafcc10f6c02
hamzafarhan07 <96057311+hamzafarhan07@users.noreply.github.com>
2021-12-13 18:04:46 +0500

    Add files via upload

---
+++ ALPHABETS.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1 As String
+        Dim alphabets As String
+        Dim char1 As Char
+        Dim i As Integer
+        Dim containallalphabets As Boolean
+        str1 = ""
+        alphabets = "abcdefghijklmnopqrstuvwxyz"
+        char1 = ""
+        i = 0
+        containallalphabets = True
+
+        Console.Write("Enter string to process :")
+        str1 = Console.ReadLine
+        str1 = LCase(str1)
+
+        For i = 1 To Len(str1)
+            char1 = Mid(alphabets, i, 1)
+            If InStr(str1, char1) = 0 Then
+                containallalphabets = False
+            End If
+            If containallalphabets = False Then
+                Exit For
+            End If
+        Next
+        If containallalphabets = True Then
+            Console.WriteLine("entered stirng contains all alphabets")
+        End If
+        If containallalphabets = False Then
+            Console.WriteLine("entered string does not contain all alphabets")
+        End If
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
---
+++ replace char.vb
@@ -0,0 +1,41 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1 As String
+        Dim str2 As String
+        Dim char1 As Char
+        Dim char2 As Char
+        Dim char3 As Char
+        Dim i As Integer
+
+        str1 = ""
+        str2 = ""
+        char1 = ""
+        char2 = ""
+        char3 = ""
+
+        Console.Write("Enter string to process :")
+        str1 = Console.ReadLine
+
+
+
+        Console.Write("Enter character that should be replaced :")
+        char1 = Console.ReadLine
+
+        Console.Write("Enter character that should replace :")
+        char3 = Console.ReadLine
+
+        For i = 1 To Len(str1)
+            char2 = Mid(str1, i, 1)
+            If char1 <> char2 Then
+                str2 = str2 & char2
+            Else : str2 = str2 & char3
+            End If
+        Next i
+        Console.WriteLine("Enter string that is processed: " & str2)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
